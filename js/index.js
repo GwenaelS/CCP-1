8aa02a19aeb78753813440e96fa71d0e28005c88
Gwenael Samyn <samyngwenaelpro@gmail.com>
2024-04-09 11:26:08 +0200

    replace navbar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const menuBurger = document.querySelector(".menu-burger")
-const navUL = document.getElementsByClassName("nav-ul")[0]
+// const menuBurger = document.querySelector(".menu-burger")
+// const navUL = document.getElementsByClassName("nav-ul")[0]
 
-menuBurger.addEventListener("click", function() {
-    navUL.classList.toggle("showmenu")
-    console.log(navUL.classList);
-})
+// menuBurger.addEventListener("click", function() {
+//     navUL.classList.toggle("showmenu")
+//     console.log(navUL.classList);
+// })
